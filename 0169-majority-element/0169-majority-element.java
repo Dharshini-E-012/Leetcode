4e5252c9b9feeccf86e7b7a888c2b35f61f48812
Dharshini <dharshu1711@gmail.com>
2026-04-11 11:08:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+            if(count>nums.length/2){
+                res=nums[i];
+                break;
+            }
+        }
+        return res;
+    }
+}
