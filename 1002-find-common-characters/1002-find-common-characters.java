3830ff08a7a20427a06a5ec32eee9241cc5cf4e1
Dharshini Eswaran <dharshu1711@gmail.com>
2026-09-02 15:03:56 +0530

    Time: 3 ms (93.54%), Space: 44.9 MB (27.34%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public List<String> commonChars(String[] words) {
+        List<String> result = new ArrayList<>();
+
+        int[] common = new int[26];
+
+        for (char ch : words[0].toCharArray()) {
+            common[ch - 'a']++;
+        }
+
+        for (int i = 1; i < words.length; i++) {
+            int[] count = new int[26];
+
+            for (char ch : words[i].toCharArray()) {
+                count[ch - 'a']++;
+            }
+
+            for (int j = 0; j < 26; j++) {
+                common[j] = Math.min(common[j], count[j]);
+            }
+        }
+
+        for (int i = 0; i < 26; i++) {
+            while (common[i] > 0) {
+                result.add(String.valueOf((char) ('a' + i)));
+                common[i]--;
+            }
+        }
+
+        return result;
+    }
+}
