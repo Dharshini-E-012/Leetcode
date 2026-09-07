4be2f4c3feb92fc7e43516bbdd6f7ca120664cf4
Dharshini Eswaran <dharshu1711@gmail.com>
2026-09-07 09:43:13 +0530

    Time: 5 ms (21.85%), Space: 46.7 MB (28.32%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,36 +1,38 @@
 class Solution {
-    public List<List<String>> res=new ArrayList<>();
+    public List<List<String>> list=new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        char[][] board=new char[n][n];
-        for(int i=0;i<board.length;i++){
-            Arrays.fill(board[i],'.');
-        }
-        backtrack(board,0);
-        return res;
-    }
-    public void backtrack(char[][] board,int row){
-        if(row==board.length){
-            ArrayList<String> l=new ArrayList<>();
-            for(int i=0;i<board.length;i++){
-                l.add(new String(board[i]));
+        char[][] arr=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                Arrays.fill(arr[i],'.');
             }
-            res.add(l);
+        }
+        backtrack(arr,0);
+        return list;
+    }
+    public void backtrack(char[][] arr,int row){
+        if(row==arr.length){
+            ArrayList<String> l=new ArrayList<>();
+            for(int i=0;i<arr.length;i++){
+                l.add(new String(arr[i]));
+            }
+            list.add(l);
             return;
         }
-        for(int col=0;col<board[row].length;col++){
-            if(isSafe(board,row,col)){
-                board[row][col]='Q';
-                backtrack(board,row+1);
-                board[row][col]='.';
+        for(int col=0;col<arr[row].length;col++){
+            if(issafe(arr,row,col)){
+                arr[row][col]='Q';
+                backtrack(arr,row+1);
+                arr[row][col]='.';
             }
         }
     }
-    public boolean isSafe(char[][] board,int row,int col){
+    public boolean issafe(char[][] arr,int row,int col){
         for(int i=0;i<row;i++){
-            if(board[i][col]=='Q')
+            if(arr[i][col]=='Q')
                 return false;
-            for(int j=0;j<board[row].length;j++){
-                if(board[i][j]=='Q'){
+            for(int j=0;j<arr[row].length;j++){
+                if(arr[i][j]=='Q'){
                     if(Math.abs(row-i)==Math.abs(col-j))
                         return false;
                 }
